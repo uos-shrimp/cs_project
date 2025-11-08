3cffaa1bff08532a09135c3ee6f87f6246ee88ae
shrimp0326 <junhyeok060326@gmail.com>
2025-11-08 19:48:16 +0900

    Connect to Firebase project

--- app/src/main/java/org/techtown/cs_project/LoginOtpActivity.java
+++ app/src/main/java/org/techtown/cs_project/LoginOtpActivity.java
@@ -10,6 +10,11 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class LoginOtpActivity extends AppCompatActivity {
 
     String phoneNumber;
@@ -22,6 +27,9 @@ public class LoginOtpActivity extends AppCompatActivity {
         phoneNumber = getIntent().getExtras().getString("phone");   //전 액비티비티에서 phone이라는 키로 넘겨준 extra 정보를 읽어와라
 
         Toast.makeText(getApplicationContext(),  phoneNumber, Toast.LENGTH_LONG).show();
+
+        Map<String, String> data = new HashMap<>();
+        FirebaseFirestore.getInstance().collection("test").add(data);
 
     }
 }
