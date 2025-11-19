ab82d31f7d8c2dafa38ef5eff428a5e62febf7b9
shrimp0326 <junhyeok060326@gmail.com>
2025-11-19 22:44:20 +0900

    ChatRoom Implement

--- app/src/main/java/org/techtown/cs_project/ChatActivity.java
+++ app/src/main/java/org/techtown/cs_project/ChatActivity.java
@@ -6,18 +6,30 @@ import android.widget.ImageButton;
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.Timestamp;
+import com.google.firebase.firestore.DocumentSnapshot;
+
+import org.techtown.cs_project.model.ChatroomModel;
 import org.techtown.cs_project.model.UserModel;
 import org.techtown.cs_project.utils.AndroidUtil;
+import org.techtown.cs_project.utils.FirebaseUtil;
+
+import java.util.Arrays;
 
 public class ChatActivity extends AppCompatActivity {
 
     UserModel otherUser;
+    String chatroomId;
+    ChatroomModel chatroomModel;
 
     EditText messageInput;
     ImageButton sendMessageBtn;
@@ -32,6 +44,7 @@ public class ChatActivity extends AppCompatActivity {
 
         //get UserModel
         otherUser = AndroidUtil.getUserModelFromIntent(getIntent());
+        chatroomId = FirebaseUtil.getChatroomId(FirebaseUtil.currentUserId(), otherUser.getUserId());
 
         messageInput = findViewById(R.id.chat_message_input);
         sendMessageBtn = findViewById(R.id.message_send_btn);
@@ -43,5 +56,26 @@ public class ChatActivity extends AppCompatActivity {
             onBackPressed();
         });
         otherUsername.setText(otherUser.getUsername());
+
+        getOnCreateChatroomModel();
     }
+
+    void getOnCreateChatroomModel() {
+        FirebaseUtil.getChatroomReference(chatroomId).get().addOnCompleteListener(task -> {
+            if(task.isSuccessful()) {
+                chatroomModel = task.getResult().toObject(ChatroomModel.class);
+                if(chatroomModel == null) {
+                    //first time chat
+                    chatroomModel = new ChatroomModel(
+                            chatroomId,
+                            Arrays.asList(FirebaseUtil.currentUserId(),otherUser.getUserId()),
+                            Timestamp.now(),
+                            ""
+                    );
+                    FirebaseUtil.getChatroomReference(chatroomId).set(chatroomModel);
+                }
+            }
+        });
+    }
+
 }
--- app/src/main/java/org/techtown/cs_project/utils/FirebaseUtil.java
+++ app/src/main/java/org/techtown/cs_project/utils/FirebaseUtil.java
@@ -26,4 +26,17 @@ public class FirebaseUtil {
         return FirebaseFirestore.getInstance().collection("users");
     }
 
+    public static DocumentReference getChatroomReference(String chatroomId) {
+        return FirebaseFirestore.getInstance().collection("chatrooms").document(chatroomId);
+    }
+
+    public static String getChatroomId(String userId1, String userId2) {
+        if(userId1.hashCode()<userId2.hashCode()) {
+            return userId1 + "_" + userId2;
+        }
+        else {
+            return userId2 + "_" + userId1;
+        }
+    }
+
 }
